09211dd9802e2798188c80923a55227402e35b9a
Niplhu <alexutiel01@gmail.com>
2026-03-17 11:44:29 +0100

    changes

--- models/website_page.py
+++ models/website_page.py
@@ -8,12 +8,10 @@ class WebsitePage(models.Model):
         self.ensure_one()
         action = self.env.ref("website_page_clone.action_website_page_clone_wizard").read()[0]
         action["context"] = {
-<<<<<<< HEAD
             "active_model": "website.page",
             "active_id": self.id,
+            "active_ids": [self.id],
             "default_source_page_id": self.id,
-=======
->>>>>>> ac08f3c837d4f92a32837a5bd9fb1484fd2b5893
             "default_source_website_id": self.website_id.id,
             "default_target_website_id": self.website_id.id,
         }
